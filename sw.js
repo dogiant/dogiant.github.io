51e2a80d5e1dd2a7267678ef4d28a376b3fac2c0
杜标棋 <dubiaoqi@jd.com>
2018-08-16 16:28:24 +0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-5615e0ed15c34c64272c.js","158672d3d34354831e378ec5c8640230"],["/app-aac0a621eae79f082a44.js","fc92ea17083f0076ecbd9bc279d1adc3"],["/app-c63fb215759d94d68896.js","3326475bbac64961cdb976cc72c1069a"],["/commons-39b20ebe64a525b95219.js","f1336abb186155c9d849e6883736a009"],["/index.html","77b3fc7985c4ce364f71c017edf0847a"],["/offline-plugin-app-shell-fallback/index.html","9d4259bbec0c0003ac6e981932d389a6"]];
+var precacheConfig = [["/app-5615e0ed15c34c64272c.js","158672d3d34354831e378ec5c8640230"],["/app-aac0a621eae79f082a44.js","fc92ea17083f0076ecbd9bc279d1adc3"],["/app-c63fb215759d94d68896.js","3326475bbac64961cdb976cc72c1069a"],["/commons-39b20ebe64a525b95219.js","f1336abb186155c9d849e6883736a009"],["/index.html","242e37ed6bbd62b5f5228db08d45485a"],["/offline-plugin-app-shell-fallback/index.html","f2b3dd5d9f52e84e8be62f32e29c9111"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
